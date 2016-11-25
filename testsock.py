67befea09365949b80147deff62ae865e86b968c
kevinnguyeneng <kevin.nguyen.eng@gmail.com>
2016-11-26 01:01:29 +0700

    refactor pppoeinit.py

--- testsock.py
+++ testsock.py
@@ -34,14 +34,14 @@ class Polserv(object):
                 try:
                     pf = json.loads(data)
                     #run_ppp(userName=pf["username"], password=pf["password"], vlanID=pf["vlanID"])
-                    conn.sendall(json.dumps({"Result": "Success"}))
+                    #conn.sendall(json.dumps({"Result": "Success"}))
                     conn.close()
                     self.numthreads -= 1
                     break
                 except:
                     conn.sendall("there error in request data")
                     conn.close()
-                    self.numthreads -=1
+                    self.numthreads -= 1
                     break
                 #conn.sendall(b"<?xml version='1.0'?><cross-domain-policy><allow-access-from domain='*' to-ports='*'/></cross-domain-policy>")
                 #conn.close()
