f8b59960ce1e604f5addeb1c56fb573ba21927f1
kevinnguyeneng <kevin.nguyen.eng@gmail.com>
2016-11-26 15:56:42 +0700

    refactor pppoeinit.py

--- testsock.py
+++ testsock.py
@@ -35,9 +35,9 @@ class Polserv(object):
             else:
                 #conn.sendall("received data %s " % data)
                 with cgi.FieldStorage() as fs:
-                    print fs.getfirst("username", "sgdsl-testload-355")
+                    print fs.getfirst("username", "sgdsl-testload-345")
                 try:
-                    #form = cgi.parse_multipart(data,)
+                    #form = cgi.parse_multipart(data)
                     #run_ppp(userName=form.getfirst("username", "Sgdsl-testload-355"),
                     #        password=form.getfirst("password", "123456"),
                     #        vlanID=form.getfirst("vlanID", "100"))
